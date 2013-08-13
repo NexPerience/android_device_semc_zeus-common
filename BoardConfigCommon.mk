3c0fc3266c1cbde2e792c6cf109431a03f35de93
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2013-08-13 03:31:49 -0500

    first commit proper for 4.3

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,48 @@
+# Copyright (C) 2011 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+-include device/semc/msm7x30-common/BoardConfigCommon.mk
+
+TARGET_BOOTLOADER_BOARD_NAME := zeus
+
+# Wifi
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
+BOARD_WLAN_DEVICE := bcm4329
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/bcm4329/parameters/firmware_path"
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/bcm4329.ko"
+WIFI_DRIVER_FW_PATH_STA := "/vendor/firmware/fw_bcm4329.bin"
+WIFI_DRIVER_FW_PATH_AP := "/vendor/firmware/fw_bcm4329_apsta.bin"
+WIFI_DRIVER_MODULE_NAME := "bcm4329"
+WIFI_DRIVER_MODULE_ARG := "firmware_path=/vendor/firmware/fw_bcm4329.bin nvram_path=/etc/wifi/calibration iface_name=wlan"
+BOARD_WLAN_DEVICE_REV := bcm4329
+WIFI_BAND := 802_11_ABG
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH_BCM := true
+TARGET_NEEDS_BLUETOOTH_INIT_DELAY := true
+
+# Camera
+BOARD_OVERLAY_FORMAT_YCbCr_420_SP := true
+BOARD_CAMERA_USE_GETBUFFERINFO := true
+
+# Kernel target toolchain
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
+
+# DASH Proximity Sensor
+SOMC_CFG_SENSORS_COMPASS_AK8975 := yes
+SOMC_CFG_SENSORS_ACCEL_BMA150_INPUT := yes
+SOMC_CFG_SENSORS_PROXIMITY_SHARP_GP2 := yes
+
+-include device/semc/msm7x30-common/Android.mk
---
+++ zeus.mk
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2011 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+-include device/semc/msm7x30-common/msm7x30.mk
+
+DEVICE_PACKAGE_OVERLAYS += device/semc/zeus-common/overlay
+
+# Wifi AP
+PRODUCT_PACKAGES += \
+    hostapd
+
+#device specific features
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
+
+# Init files
+PRODUCT_COPY_FILES += \
+    device/semc/zeus-common/prebuilt/init.semc.rc:root/init.semc.rc
+
+# Touchpad Activator apk - Thanks @Farenheith -- full credits
+PRODUCT_COPY_FILES += \
+    device/semc/zeus-common/prebuilt/TouchpadActivator_V_1_3.apk:system/app/TouchpadActivator_V_1_3.apk
+
+# Common device properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    wifi.interface=wlan0 \
+    com.qc.hdmi_out=false
